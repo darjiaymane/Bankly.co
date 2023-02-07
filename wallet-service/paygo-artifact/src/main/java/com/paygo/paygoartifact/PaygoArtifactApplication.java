792f7007d30a89e1db0b4e95c7172476e032941c
TayebsouiniYcode <TayebsouiniYcode@gmail.com>
2023-02-07 15:20:14 +0100

    ajouter les dependences pour eureka clients wallet et transaction service

--- wallet-service/paygo-artifact/src/main/java/com/paygo/paygoartifact/PaygoArtifactApplication.java
+++ wallet-service/paygo-artifact/src/main/java/com/paygo/paygoartifact/PaygoArtifactApplication.java
@@ -2,8 +2,11 @@ package com.paygo.paygoartifact;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class PaygoArtifactApplication {
 
 	public static void main(String[] args) {
