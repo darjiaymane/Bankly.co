7809b267e48d52aa6bb36d25c6d4664e52e784d9
TayebsouiniYcode <TayebsouiniYcode@gmail.com>
2023-02-08 09:11:37 +0100

    termine la partie eureka

--- transaction-service/paygo/src/main/java/com/paygo/paygo/PaygoApplication.java
+++ transaction-service/paygo/src/main/java/com/paygo/paygo/PaygoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
-@SpringBootApplication
+@SpringBootApplication //TODO enableeurekaclient
 public class PaygoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PaygoApplication.class, args);
