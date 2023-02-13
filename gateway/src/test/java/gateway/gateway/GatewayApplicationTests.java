85280658f363ee8f87c2f5922c0c8ad326c0347e
TayebsouiniYcode <TayebsouiniYcode@gmail.com>
2023-02-13 10:13:11 +0100

    initial commit gateway

--- gateway/src/test/java/gateway/gateway/GatewayApplicationTests.java
+++ gateway/src/test/java/gateway/gateway/GatewayApplicationTests.java
@@ -1,13 +1,13 @@
-package com.gateway.gatewayservice;
+package gateway.gateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class GatewayServiceApplicationTests {
+class GatewayApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
